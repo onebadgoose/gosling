f4a18299da1fe3a790ff1c4f0678cf5ea141ed13
Joe Malott <joe@onebadgoose.com>
2022-03-31 17:00:51 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
-const BIIIRD_THEME_VERSION = 1.0;
+const GOSLING_THEME_VERSION = 1.0;
 
 // Enqueue Tailwind CSS
 add_action( 'wp_enqueue_scripts', 'enqueue_function', 10 );
 function enqueue_function() {
-	$version = ( wp_get_environment_type() === 'development' ) ? time() : BIIIRD_THEME_VERSION;
+	$version = ( wp_get_environment_type() === 'development' ) ? time() : GOSLING_THEME_VERSION;
 	wp_enqueue_style( 'tailwind', get_template_directory_uri() . '/assets/css/main.css', $version, true );
 }
 
