aaab048f37fff9813ed4e7a9f20df9f447787ea6
Joe Malott <joe@onebadgoose.com>
2022-03-31 17:20:51 -0500

    Now we're cooking with gas

--- functions.php
+++ functions.php
@@ -17,3 +17,9 @@ if( function_exists('acf_add_options_page') ) {
 	
 }
 
+// Removes the 28px admin bar bump when signed in
+add_action('get_header', 'my_filter_head');
+
+function my_filter_head() {
+  remove_action('wp_head', '_admin_bar_bump_cb');
+}
--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php get_header(); ?>
 
-    <div class="bg-gray-200">
-        <div class="container mx-auto mt-4">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl">Hello World!</h1>
-        </div>
-    </div>
 
-    <?php 
-        if ( have_posts() ) {
-            while ( have_posts() ) {
-                the_post(); 
-                
-                the_content();
-            } // end while
-        } // end if
-    ?>
+<!-- Start Building Your Archive pages here or style this as a fallback page when things go wrong -->
 
 <?php get_footer(); ?>
